9d690fd80c104cd2114485cbcff1de1b429f0429
Pripravar <270124376+Pripravar@users.noreply.github.com>
2026-07-08 08:37:39 +0200

    s

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@
 // DŮLEŽITÉ: HTML/navigace jde NETWORK-FIRST. Připnutá PWA na iOS si jinak
 // drží starý index.html z cache a po pushi neukazuje aktualizaci. Online
 // se vždy stáhne čerstvý HTML, offline padá zpět na poslední cache.
-var CACHE_NAME = 'martinovice-v0.11-2026-07-07';
+var CACHE_NAME = 'martinovice-v0.13-2026-07-07';
 var ASSETS = [
   './',
   './index.html',
